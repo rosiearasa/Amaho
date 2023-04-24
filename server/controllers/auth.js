0f1b598c157083cb2ead70a67dd7e1c216671669
rosiearasa <rosearasa7522@gmail.com>
2023-04-23 20:28:57 -0400

    user email authentication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,6 @@
-
+import * as config from "../config.js";
+import jwt from 'jsonwebtoken'
+import {emailTemplate} from '../helpers/email.js'
 
 export const welcome = (req, res) => {
     res.json({
@@ -10,13 +12,45 @@ export const welcome = (req, res) => {
 export const preRegister = async(req, res)=>{
     //create jwt with email and password then email as clickable link
     //only when user clicks on the email link registration completes
-    
+
     try{
-        //
-        console.log(req.body);
+        const {email,password} = req.body;
+
+        let token = jwt.sign({email, password}, config.JWT_SECRET, {
+            expiresIn: "1h",
+        });
+
+
+
+
+       config.AWSSES.sendEmail(emailTemplate(email,
+        `<p>Please click the link below to activate your account.<p>
+
+        <a href= "${config.CLIENT_URL}/auth/account-activate/${token}">Activate my account</a>`, 
+        config.REPLY_TO, "Activate your account"), (err, data)=>{
+            if(err){
+                return res.json({ok:false});
+            }
+            else{
+                console.log(data)
+                return res.json({ok:true});
+            }
+
+        });
+       
     } catch(err){
-        console.log(err)
+        console.log(err);
         return res.json({error:"Something went wrong. Try AGAIN."});
 
     }
+};
+
+export const register = async(req, res)=>{
+try{
+    console.log(req.body);
+
+} catch (err){
+    console.log(err);
+    return res.json({error: "something went wrong. Try again"})
+}
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,5 +7,6 @@ import * as auth from "../controllers/auth.js";
 router.get("/", auth.welcome);
 
 router.post('/pre-register', auth.preRegister);
+router.post('/register', auth.register)
 
 export default router
